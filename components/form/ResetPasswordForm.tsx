5317d0d05f037518e4ddbd80a2ad6f70d4a38559
Prajapati vishal <107975264+vishal007p@users.noreply.github.com>
2026-01-02 00:54:37 +0530

    fix: Update API URL for password reset functionality

--- components/form/ResetPasswordForm.tsx
+++ components/form/ResetPasswordForm.tsx
@@ -44,7 +44,7 @@ const ResetPasswordForm = () => {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+        `${process.env.NEXT_PUBLIC_QUARTUS_API_URL}/auth/reset-password`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
